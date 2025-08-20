191c42230001b458bed4002a24daadf4da2189c5
babakyusifov <babek@div.edu.az>
2025-08-20 20:17:54 +0400

    0-classroom.js

--- ES6_classes/0-classroom.js
+++ ES6_classes/0-classroom.js
@@ -1,9 +1,9 @@
-/* Classroom
-    https://stackoverflow.com/questions/4484424/underscore-prefix-for-property-and-method-names-in-javascript
-    prefix
-*/
+// Classroom class
+// Mənbə: https://stackoverflow.com/questions/4484424/underscore-prefix-for-property-and-method-names-in-javascript
+
 export default class ClassRoom {
   constructor(maxStudentsSize = 0) {
+    // _maxStudentsSize adlandırılması onun private və ya daxili istifadə üçün olduğunu göstərir (konvensiya olaraq)
     this._maxStudentsSize = maxStudentsSize;
   }
 }
