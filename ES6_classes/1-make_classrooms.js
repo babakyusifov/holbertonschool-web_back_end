e5f3cfa81502d49afb99cb291687b90935aed993
babakyusifov <babek@div.edu.az>
2025-08-20 20:29:54 +0400

    1-make_classrooms.js

--- ES6_classes/1-make_classrooms.js
+++ ES6_classes/1-make_classrooms.js
@@ -1,46 +1,9 @@
-// Initialize size of classroom
 import ClassRoom from './0-classroom';
 
+/**
+ * Creates an array of {@link ClassRoom}s with a specific size.
+ * @returns An array of {@link ClassRoom}s.
+ */
 export default function initializeRooms() {
-  return [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)];
-}// Initialize size of classroom
-import ClassRoom from './0-classroom';
-
-export default function initializeRooms() {
-	  return [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)];
-}// Initialize size of classroom
-import ClassRoom from './0-classroom';
-
-export default function initializeRooms() {
-	  return [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)];
-}// Initialize size of classroom
-import ClassRoom from './0-classroom';
-
-export default function initializeRooms() {
-	  return [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)];
-}// Initialize size of classroom
-import ClassRoom from './0-classroom';
-
-export default function initializeRooms() {
-	  return [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)];
-}// Initialize size of classroom
-import ClassRoom from './0-classroom';
-
-export default function initializeRooms() {
-	  return [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)];
-}// Initialize size of classroom
-import ClassRoom from './0-classroom';
-
-export default function initializeRooms() {
-	  return [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)];
-}// Initialize size of classroom
-import ClassRoom from './0-classroom';
-
-export default function initializeRooms() {
-	  return [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)];
-}// Initialize size of classroom
-import ClassRoom from './0-classroom';
-
-export default function initializeRooms() {
-	  return [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)];
+  return [19, 20, 34].map((size) => new ClassRoom(size));
 }
